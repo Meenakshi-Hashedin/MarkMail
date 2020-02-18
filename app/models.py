6f455b9306853b1c865247a507ef2fac1160acb6
meenakshi.d <meenakshi.d@gmail.com>
2020-02-18 18:41:20 +0530

    initial commit

--- app/models.py
+++ app/models.py
@@ -1,13 +1,19 @@
 from app import db
+
+
 class User(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
-    username = db.Column(db.String(64),unique=True)
-    email=db.Column(db.String(120),unique=True)
-    password_hash=db.Column(db.String(128))
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+    __tablename__ = 'user'
 
 
-    __tablename__ = 'CustomUser'
+class MyTemplate(db.Model):
+    content = db.Column(db.String(500))
+
+    ____tablename__ = 'temp'
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
-db.create_all()
+
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,63 @@
-from flask import Flask,request,jsonify,make_response
-from app.models import User
+from flask import Flask, request, jsonify
+from app.models import User,db
 from app import app
 @app.route('/hello-world')
 def hello_world():
     return 'Hello world'
+
+@app.route('/users',methods=['POST','GET','DELETE','PUT'])
+def User_Data():
+
+    if request.method == 'POST':
+        username = request.args.get('username')
+        email = request.args.get('email')
+        user = User(username=username, email=email)
+        db.session.add(user)
+        db.session.commit()
+        return '{}{}' .format(username, email)
+
+    if request.method == 'GET':
+        username = request.args.get('username')
+        email = request.args.get('email')
+        return 'userDetails {}{}' .format(username, email)
+
+    if request.method == 'PUT':
+        username = request.args.get('username')
+        email = request.args.get('email')
+        editName='sarada'
+        user = User.query.filter_by(username=username).first()
+        user.username = editName
+        db.session.commit()
+        return 'updated name is{}' + editName
+
+    if request.method == 'DELETE':
+        username = request.args.get('username')
+        email = request.args.get('email')
+        User.query.filter_by(username=username,email=email).delete()
+        db.session.commit()
+        return 'deleted {}' .format(username)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
